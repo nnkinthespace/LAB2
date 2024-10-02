4f914ca1f3dd03feaf52ce6bf3b34930f11e0f16
khangnguyen2k4 <khang.nguyen2k4@hcmut.edu.vn>
2024-10-02 23:56:43 +0700

    init ex5

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -146,6 +146,16 @@ void display7SEG(int num) {
  * @brief  The application entry point.
  * @retval int
  */
+int hour = 15, minute = 8, second = 50;
+int led_buffer[4];
+void updateClockBuffer()
+{
+	led_buffer[0] = hour / 10;
+	led_buffer[1] = hour % 10;
+	led_buffer[2] = minute / 10;
+	led_buffer[3] = minute % 10;
+}
+
 int main(void) {
 	/* USER CODE BEGIN 1 */
 
@@ -180,7 +190,20 @@ int main(void) {
 	setTimer2(25);
 		while (1) {
 		/* USER CODE END WHILE */
-
+			 second++;
+			    if (second >= 60){
+			        second = 0;
+			        minute++;
+			    }
+			    if(minute >= 60){
+			        minute = 0;
+			        hour++;
+			    }
+			    if(hour >=24){
+			        hour = 0;
+			    }
+			    updateClockBuffer();
+			    HAL_Delay(1000);
 		/* USER CODE BEGIN 3 */
 	}
 	/* USER CODE END 3 */
@@ -311,7 +334,6 @@ void clear() {
 }
 const int MAX_LED = 4;
 int index_led = 0;
-int led_buffer[4] = {1, 2, 3, 0};
 int Pin[4] = { EN0_Pin, EN1_Pin, EN2_Pin, EN3_Pin};
 void enPin(int en)
 {
