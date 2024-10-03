497dc4750147f258f9589b456496ed384d5d4a8e
khangnguyen2k4 <khang.nguyen2k4@hcmut.edu.vn>
2024-10-03 16:55:01 +0700

    update ex9

--- STM32/Core/Src/software_timer.c
+++ STM32/Core/Src/software_timer.c
@@ -14,6 +14,9 @@ int timer2_flag = 0;
 
 int timer3_counter = 0;
 int timer3_flag = 0;
+
+int timer4_counter = 0;
+int timer4_flag = 0;
 void setTimer1(int duration) {
 	timer1_counter = duration;
 	timer1_flag = 0;
@@ -27,6 +30,11 @@ void setTimer2(int duration) {
 void setTimer3(int duration) {
 	timer3_counter = duration;
 	timer3_flag = 0;
+}
+
+void setTimer4(int duration) {
+	timer4_counter = duration;
+	timer4_flag = 0;
 }
 
 void TimerRun() {
@@ -50,5 +58,13 @@ void TimerRun() {
 			timer3_flag = 1;
 		}
 	}
+
+	if (timer4_counter > 0) {
+		timer4_counter--;
+		if (timer4_counter == 0) {
+			timer4_flag = 1;
+		}
+	}
+
 }
 
